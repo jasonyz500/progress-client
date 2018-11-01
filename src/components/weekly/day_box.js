007f01372526564408d31f5f436fb791e54a6569
Jason Zhou <jzhou@pinterest.com>
2018-10-31 22:32:29 -0700

    clean up day box

--- src/components/weekly/day_box.js
+++ src/components/weekly/day_box.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Box, Button, Column, Divider, Heading, Icon, IconButton, Label, Modal, Text, TextArea } from 'gestalt';
+import { Box, Divider, Icon, IconButton, Text } from 'gestalt';
 import moment from 'moment';
 import _ from 'lodash';
-import { WithContext as ReactTags } from 'react-tag-input';
 import { moodToColorMap } from '../common/constants';
 import { drawTags } from '../common/utils';
 import { createEntry, updateEntry, getTags } from '../../actions/';
@@ -14,18 +13,9 @@ class DayBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal: false,
-      dateStr: props.dateStr,
-      modalEntry: {
-        mood_score: 0,
-        mood_reason: '',
-        updates: []
-      }
+      dateStr: props.dateStr
     };
-    this.handleMoodSelect.bind(this);
   }
-
-  /* FUNCTIONS FOR PARENT DATE BOX */
 
   drawEntryContent(entry) {
     // if(_.isEmpty(entry)) {
@@ -52,200 +42,6 @@ class DayBox extends Component {
           <Text key={i} align="left">- {update.body} {drawTags(update.tags)}</Text>
         ))}
       </Box>
-    );
-  }
-
-  /* END FUNCTIONS FOR PARENT DATE BOX */
-  /* FUNCTIONS FOR MODAL */
-
-  toggleModal() {
-    this.props.getTags();
-    this.setState(prevState => ({ 
-      showModal: !prevState.showModal,
-      modalEntry: _.cloneDeep(this.props.entry)
-    }));
-  }
-
-  handleMoodSelect(mood) {
-    const { modalEntry } = this.state;
-    modalEntry.mood_score = mood;
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  handleMoodReason({ value }) {
-    const { modalEntry } = this.state;
-    modalEntry.mood_reason = value;
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  drawUpdate(update, idx) {
-    const tags = _.map(update.tags, tag => ({ text: tag.tag, id: (tag.id || 'null').toString() }));
-    return (
-      <Box key={idx}>
-        <Box paddingY={2} paddingX={4} display="flex" alignItems="center">
-          <Column span={4}>
-            <Label htmlFor={`update${idx}`}>
-              <Text align="left" bold>Update</Text>
-            </Label>
-          </Column>
-          <Column span={8}>
-            <TextArea 
-              id={`update${idx}`}
-              value={this.state.modalEntry.updates[idx].body}
-              onChange={(e) => this.handleChangeUpdate(e.value, idx)}
-            />
-          </Column>
-        </Box>
-        <Box paddingY={2} paddingX={4} display="flex" alignItems="center">
-          <Column span={4}>
-            <Label htmlFor={`tags${idx}`}>
-              <Text align="left" bold>Tags</Text>
-            </Label>
-          </Column>
-          <Column span={8}>
-            <ReactTags
-              tags={tags}
-              handleDelete={(e) => this.handleDeleteTag(e, idx)}
-              handleAddition={(e) => this.handleAddTag(e, idx)}
-              suggestions={this.props.tags}
-            />
-          </Column>
-        </Box>
-        <Box display="flex" direction="row" justifyContent="end" right>
-          <IconButton
-            accessibilityLabel="Delete"
-            icon="remove"
-            iconColor="red"
-            onClick={() => this.removeUpdate(idx)}
-          />
-        </Box>
-        <Divider />
-      </Box>
-    );
-  }
-
-  addUpdate() {
-    const { modalEntry } = this.state;
-    modalEntry.updates.push({
-      body: '',
-      tags: []
-    });
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  removeUpdate(idx) {
-    const { modalEntry } = this.state;
-    modalEntry.updates = modalEntry.updates.filter((update, i) => i !== idx)
-    this.setState(prevState => ({ modalEntry: modalEntry })); 
-  }
-
-  handleChangeUpdate(body, idx) {
-    const { modalEntry } = this.state;
-    modalEntry.updates[idx].body = body;
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  handleAddTag(tag, idx) {
-    const { modalEntry } = this.state;
-    modalEntry.updates[idx].tags.push({ tag: tag.text, id: null });
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  handleDeleteTag(tagIdx, updateIdx) {
-    const { modalEntry } = this.state;
-    modalEntry.updates[updateIdx].tags = modalEntry.updates[updateIdx].tags.filter((tag, i) => i !== tagIdx);
-    this.setState(prevState => ({ modalEntry: modalEntry }));
-  }
-
-  handleSave() {
-    const { modalEntry } = this.state;
-    if(!modalEntry.id) {
-      this.props.createEntry(modalEntry, () => window.location.reload());
-    } else {
-      this.props.updateEntry(modalEntry, () => window.location.reload());
-    }
-  }
-
-  /* END FUNCTIONS FOR MODAL */
-
-  drawEditModal() {
-    const { dateStr, modalEntry } = this.state;
-    return (
-      <Modal
-        accessibilityCloseLabel="close"
-        accessibilityModalLabel="edit"
-        heading={moment(dateStr).format('dddd, MMMM Do')}
-        onDismiss={this.toggleModal.bind(this)}
-        size="lg"
-        footer={
-          <Box
-            display="flex"
-            direction="row"
-            justifyContent="end"
-          >
-            <Box paddingX={1}>
-              <Button text="Cancel" inline onClick={this.toggleModal.bind(this)} />
-            </Box>
-            <Button color="red" inline text="Save" onClick={this.handleSave.bind(this)}/>
-          </Box>
-        }
-      >
-        <Box display="flex" direction="row" position="relative" alignItems="center">
-          <Column span={12}>
-            <Box paddingY={2} paddingX={4} display="flex" alignItems="center">
-              <Column span={4}>
-                <Label htmlFor="mood_score">
-                  <Text align="left" bold>Mood Score</Text>
-                </Label>
-              </Column>
-              <Column span={8}>
-                <Box display="flex" direction="row">
-                {
-                  [...Array(5).keys()].map(i => (
-                    <IconButton
-                      key={i}
-                      accessibilityLabel={`Select ${i+1} Stars`}
-                      icon="smiley-outline"
-                      iconColor={modalEntry && modalEntry.mood_score > i ? 'red' : 'gray'}
-                      onClick={() => this.handleMoodSelect(i+1)}
-                    />
-                  ))
-                }
-                </Box>
-              </Column>
-            </Box>
-            <Box paddingY={2} paddingX={4} display="flex" alignItems="center">
-              <Column span={4}>
-                <Label htmlFor="mood_reason">
-                  <Text align="left" bold>Mood Reason</Text>
-                </Label>
-              </Column>
-              <Column span={8}>
-                <TextArea
-                  id="mood_reason"
-                  onChange={this.handleMoodReason.bind(this)}
-                  value={modalEntry.mood_reason}
-                  placeholder="Describe your mood"
-                  rows={2}
-                />
-              </Column>
-            </Box>
-            <Divider />
-            <Box paddingY={2} paddingX={4} display="flex">
-              <Heading size="xs">Project Updates</Heading>
-            </Box>
-            {_.map(modalEntry.updates, (update, i) => this.drawUpdate(update, i))}
-            <Text align="left" bold>Add an Update</Text>
-            <IconButton
-              accessibilityLabel="add an update"
-              icon="add-circle"
-              iconColor="red"
-              size="xl"
-              onClick={this.addUpdate.bind(this)}
-            />
-          </Column>
-        </Box>
-      </Modal>
     );
   }
 
@@ -280,7 +76,6 @@ class DayBox extends Component {
         <Box color="white" padding={2}>
           {this.drawEntryContent(entry)}
         </Box>
-        {this.state.showModal && this.drawEditModal()}
       </Box>
     );
 	}
