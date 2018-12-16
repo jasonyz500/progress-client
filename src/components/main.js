3da0aee5154df76dd19b9ace6a86bcf70a689f6a
Jason Zhou <zhou.jasony@gmail.com>
2018-12-15 22:00:10 -0800

    password reset form

--- src/components/main.js
+++ src/components/main.js
@@ -7,6 +7,7 @@ import Yearly from './yearly/';
 import EditWeeklyUpdates from './edit/edit_weekly_updates';
 import EditDailyEntry from './edit/edit_daily_entry';
 import Profile from './profile';
+import PasswordReset from './unauth/password_reset';
 
 const Main = () => ( // todo: add validation for edit/weekly/:weekStr param
   <main>
@@ -19,6 +20,7 @@ const Main = () => ( // todo: add validation for edit/weekly/:weekStr param
       <Route path='/edit/weekly/:weekStr' component={EditWeeklyUpdates}/>
       <Route path='/edit/daily/:dateStr' component={EditDailyEntry}/>
       <Route path='/profile' component={Profile}/>
+      <Route path='/password_reset' component={PasswordReset}/>
     </Switch>
   </main>
 )
