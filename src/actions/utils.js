74eb57128e6cd283109ba910148d5da8ac355d53
Jason Zhou <jzhou@pinterest.com>
2018-12-24 12:00:13 -0800

    change config

--- src/actions/utils.js
+++ src/actions/utils.js
@@ -1,4 +1,4 @@
-export const ROOT_URL = process.env.NODE_ENV === 'production' ? 'http://progress-svc.herokuapp.com' : 'localhost:3000';
+export const ROOT_URL = process.env.NODE_ENV === 'production' ? 'http://progress-svc.herokuapp.com' : 'http://localhost:3000';
 
 export function getConfig() {
   return {
