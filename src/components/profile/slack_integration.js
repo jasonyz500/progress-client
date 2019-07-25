5562cac10213c3eec300638003c5299f3b48174b
Jason Zhou <jzhou@pinterest.com>
2019-07-24 17:30:55 -0700

    slack stuff

--- src/components/profile/slack_integration.js
+++ src/components/profile/slack_integration.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { Box, Button, Heading, Text } from 'gestalt';
 
 const INTEGRATION_URL = 'https://slack.com/oauth/authorize?';
+export const REDIRECT_URL = process.env.NODE_ENV === 'production' ? 'https%3A%2F%2Fprogress-svc.herokuapp.com' : 'http%3A%2F%2Flocalhost%3A5000' + '%2Fprofile%2Fslack_redirect';
 
 class SlackIntegration extends Component {
 
   handleAddIntegration() {
-    window.location.assign(`${INTEGRATION_URL}client_id=2156640555.646972247700&scope=chat:write:bot&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fprofile%2Fslack_redirect`);
+    window.location.assign(`${INTEGRATION_URL}scope=identity.basic&client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&redirect_uri=${REDIRECT_URL}&state=${process.env.REACT_APP_SLACK_STATE}`);
   }
 
   render() {
+    const isEncryptionEnabled = localStorage.getItem('is_encryption_enabled') === 'true';
+
     return (
       <div>
       <Box paddingY={2}>
@@ -17,12 +20,19 @@ class SlackIntegration extends Component {
       </Box>
       <Text>Fill in your daily entries via text your workplace's slack.</Text>
       <Box paddingY={2}>
-        <Button
-          text="Add Slack Integration"
-          onClick={this.handleAddIntegration}
-          color="red"
-          inline
-        />
+        {isEncryptionEnabled && (
+          <div>
+            <Text color="red">Sorry, adding slack to an encrypted account is still under development. Check back soon!</Text>
+          </div>
+        )}
+        {!isEncryptionEnabled && (
+          <Button
+            text="Add Slack Integration"
+            onClick={this.handleAddIntegration}
+            color="red"
+            inline
+          />
+        )}
       </Box>
       </div>
     );
--- src/components/profile/slack_integration_redirect.js
+++ src/components/profile/slack_integration_redirect.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import { Heading } from 'gestalt';
+import axios from 'axios';
+import { REDIRECT_URL } from './slack_integration';
+import { ROOT_URL } from '../../actions/utils';
+import queryString from 'query-string';
 
-const URL = 'https://slack.com/api/oauth.access';
+const URL = 'https://slack.com/api/oauth.access?';
 
 class SlackIntegrationRedirect extends Component {
 
   componentDidMount() {
     // send post to URL with code param, get back access token
     // save userid, bot access token, and default settings in a table
+    const params = queryString.parse(this.props.location.search);
+    if (params.state !== process.env.REACT_APP_SLACK_STATE) {
+      console.log('incorrect slack state verification code');
+      return;
+    }
+    // TODO: check if process.env variables is safe to store secret keys
+    axios.get(`${URL}client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}&client_secret=${process.env.REACT_APP_SLACK_CLIENT_SECRET}&code=${params.code}&redirect_uri=${REDIRECT_URL}`)
+      .then(res => {
+        console.log(res);
+        
+      });
   }
 
   render() {
